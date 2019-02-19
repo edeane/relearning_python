1ab4a50dd3a58ccbffaad82b25ce0f91bbbb6709
edeane <edward.deane.ii@gmail.com>
2019-02-18 23:34:02 -0700

    chugging along nicely

--- learning_numpy.py
+++ learning_numpy.py
@@ -1,15 +1,17 @@
-'''
+"""
 numpy
 https://scipy-cookbook.readthedocs.io/items/idx_numpy.html
 
 scipy
 https://scipy-cookbook.readthedocs.io/items/FortranIO.html
 
-'''
-
+"""
 
 import numpy as np
 
 
+abc = np.random.rand()
 
 
+if __name__ == '__main__':
+    print('pass')
