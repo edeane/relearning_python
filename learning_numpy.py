2811455ea13744531e8b7ff58ccdd2c475e35050
Eddie Deane <edeane@wiland.com>
2019-02-21 16:59:51 -0700

    getting through minimal pandas

--- learning_numpy.py
+++ learning_numpy.py
@@ -32,9 +32,10 @@ Basic Linear Algebra
 """
 
 import numpy as np
+import matplotlib.pyplot as plt
 
 # starting with https://docs.scipy.org/doc/numpy/user/quickstart.html
-a = np.arange(15).reshape(3,5)
+a = np.arange(15).reshape(3, 5)
 print(a.shape)
 a.ndim
 a.dtype.name
@@ -45,42 +46,39 @@ b = np.array([6, 7, 8])
 b
 type(b)
 
-c = np.array([ [1,2], [3,4] ], dtype=complex)
+c = np.array([[1, 2], [3, 4]], dtype=complex)
 c
 
-np.zeros( (3,4) )
-np.ones( (2,3,4) , dtype=np.int16)
+np.zeros((3, 4))
+np.ones((2, 3, 4), dtype=np.int16)
 
-np.empty( (2,3) )
+np.empty((2, 3))
 
 np.arange(10, 30, 5)
 np.linspace(0, 2, 9)
 
-x = np.linspace(0, 2*np.pi, 100)
+x = np.linspace(0, 2 * np.pi, 100)
 f = np.sin(x)
 f
 
-print(np.arange(10000).reshape(100,100))
+print(np.arange(10000).reshape(100, 100))
 
-a = np.array( [20,30,40,50] )
+a = np.array([20, 30, 40, 50])
 b = np.arange(4)
-a-b
-
+a - b
 
 b
-b**2
-10*np.sin(a)
+b ** 2
+10 * np.sin(a)
 a < 35
 
+a = np.array([[1, 1],
+              [0, 1]])
 
+b = np.array([[2, 0],
+              [3, 4]])
 
-a = np.array( [[1,1],
-               [0,1]])
-
-b = np.array( [[2,0],
-               [3,4]])
-
-#element wise
+# element wise
 a * b
 
 # matrix product
@@ -88,14 +86,13 @@ a @ b
 a.dot(b)
 
 # inplace
-a = np.ones((2,3),dtype=int)
+a = np.ones((2, 3), dtype=int)
 a *= 3
 a
 
-b = np.random.random((2,3))
+b = np.random.random((2, 3))
 b += a
 b
-
 
 a += b
 b.dtype
@@ -107,19 +104,17 @@ a = np.arange(11)
 
 a.tobytes()
 
-
-
 a = np.ones(3, dtype=np.int32)
 b = np.linspace(0, np.pi, 3)
 a
 b
 c = a + b
 c
-d = np.exp(c*1j)
+d = np.exp(c * 1j)
 d
 
 1j
-a = np.array([1,2,3,4,5])
+a = np.array([1, 2, 3, 4, 5])
 np.exp(a)
 
 import math
@@ -127,22 +122,19 @@ import math
 math.e ** 1
 math.e ** 2
 
-
-a = np.random.random((2,3))
+a = np.random.random((2, 3))
 a.sum()
 a.min()
 a.max()
 
-
 # axis
 
 b = np.arange(12).reshape(3, 4)
-b.sum(axis=0) # sum each column
-b.sum(axis=1) # sum each row
+b.sum(axis=0)  # sum each column
+b.sum(axis=1)  # sum each row
 b.min(axis=1)
 b.min(axis=0)
 b.cumsum(axis=1)
-
 
 b = np.arange(3)
 np.sqrt(b)
@@ -150,13 +142,12 @@ c
 b
 np.add(b, c)
 
-
-
 a = np.arange(10) ** 3
 a
 a[2]
 a[2:5]
 a[::-1]
+
 
 # row = x
 # column = y
@@ -165,7 +156,7 @@ def f(x, y):
     return (10 * x) + y
 
 
-b = np.fromfunction(f, (5,4), dtype=int)
+b = np.fromfunction(f, (5, 4), dtype=int)
 b[2, 3]
 b[0:5, 1]
 b[0:5, 0]
@@ -177,7 +168,6 @@ b[-1, :]
 
 np.array(b.flat)
 
-
 a = np.floor(10 * np.random.random((3, 4)))
 a.shape
 
@@ -188,15 +178,12 @@ a.reshape(6, 2)
 a
 a.T
 
-
-
-
-a = np.floor(10*np.random.random((2,2)))
+a = np.floor(10 * np.random.random((2, 2)))
 a
-b = np.floor(10*np.random.random((2,2)))
+b = np.floor(10 * np.random.random((2, 2)))
 b
-np.vstack((a,b))
-np.hstack((a,b))
+np.vstack((a, b))
+np.hstack((a, b))
 
 # newaxis
 
@@ -208,12 +195,11 @@ a[:, np.newaxis]
 
 from pprint import pprint
 
-a = np.floor(10*np.random.random((2,12)))
+a = np.floor(10 * np.random.random((2, 12)))
 
 pprint(np.hsplit(a, 3))
 
-pprint(np.hsplit(a, (3,4))) # after 3rd and 4th column
-
+pprint(np.hsplit(a, (3, 4)))  # after 3rd and 4th column
 
 a
 id(a)
@@ -225,22 +211,182 @@ id(a) == id(b)
 b = a[:]
 id(a) == id(b)
 
-
 b = a.copy()
 id(a) == id(b)
 a
 b
 
-a.argmax() # index of max
-a.argmin() # index of min
+a.argmax()  # index of max
+a.argmin()  # index of min
 a.argsort()
-
-
 
 # complex slicing
 
 
 # left off here https://docs.scipy.org/doc/numpy/user/quickstart.html#fancy-indexing-and-index-tricks
+
+
+a = np.arange(1, 5.5, .5)
+
+a.cumsum()
+
+a = np.arange(12) ** 2
+a
+
+idx = np.array([1, 1, 3, 8, 5])
+
+a[idx]
+
+j = np.array([[3, 4], [9, 7]])
+
+a[j]
+
+palette = np.array([[0, 0, 0],  # black
+                    [255, 0, 0],  # red
+                    [0, 255, 0],  # green
+                    [0, 0, 255],  # blue
+                    [255, 255, 255]])  # white
+
+palette
+
+image = np.array([[0, 1, 2, 0],
+                  [0, 3, 4, 0]])
+
+palette[image]
+
+time = np.linspace(20, 145, 5)
+data = np.sin(np.arange(20)).reshape(5, 4)
+time
+data
+
+# 0 = down
+# 1 = across
+
+ind = data.argmax(axis=0)
+ind
+
+time[ind]
+
+data[ind, range(4)]
+
+a = np.arange(5)
+a[[1, 2, 3]] = 0
+a
+
+a = np.arange(12).reshape(3, 4)
+a
+
+b = a > 4
+a[b] = 0
+a
+
+maxit = 20
+maxit
+
+def mandelbrot(h, w, maxit=20):
+    """Returns an image of the Mandelbrot fractal of size (h,w).
+
+    Args:
+        h: height
+        w: widht
+        maxit: max iterations
+    Returns: matpotlib image
+    """
+    y, x = np.ogrid[-1.4:1.4:h * 1j, -2:0.8:w * 1j]
+    c = x + y * 1j
+    z = c
+    divtime = maxit + np.zeros(z.shape, dtype=int)
+
+    for i in range(maxit):
+        z = z ** 2 + c
+        diverge = z * np.conj(z) > 2**2
+        div_now = diverge & (divtime == maxit)
+        divtime[div_now] = i
+        z[diverge] = 2
+
+    return divtime
+
+mand_4_4 = mandelbrot(400, 400)
+mand_4_4.shape
+
+plt.imshow(mand_4_4)
+
+
+a = np.arange(12).reshape(3,4)
+b1 = np.array([False, True, True])
+b2 = ([True, False, True, False])
+
+a
+a[b1, :]
+a[:, b2]
+a[b1, b2]
+
+# Linear Algebra
+
+a = np.arange(1, 5, dtype=float).reshape(2,2)
+a
+a.T
+a.transpose()
+np.linalg.inv(a)
+u = np.eye(2)
+u
+j = np.array([[0, -1], [1,0]], dtype=float)
+j
+j @ j
+np.trace(u)
+
+
+a = np.arange(30)
+a.reshape(2,5,3)
+a.shape = (2,5,3)
+a
+
+
+x = {'a': 1, 'b': 2}
+y = {'b': 3, 'c': 4}
+
+z = {**x, **y}
+z
+
+
+# ---- cs231 ----
+
+
+
+a_arr = [3, 8, 6, 5, 1, 2, 1]
+
+pivot = a_arr[len(a_arr) // 2]
+left = [ x for x in a_arr if x < pivot]
+middle = [x for x in a_arr if x == pivot]
+right = [x for x in a_arr if x > pivot]
+
+print(pivot, left, middle, right)
+
+len(a_arr) / 2
+
+
+
+
+np.full((2,3), 7)
+
+
+
+a = np.arange(1, 7).reshape(3, 2)
+a[a>2]
+
+
+a = np.arange(1, 5, dtype=np.float64).reshape(2, 2)
+b = np.arange(5, 9, dtype=np.float64).reshape(2, 2)
+a
+b
+a + b
+a / b
+a ** .5
+np.sqrt(a)
+
+
+
+
 
 
 
--- learning_pandas.py
+++ learning_pandas.py
@@ -1,5 +1,189 @@
-'''
+"""
 pandas
+https://medium.com/dunder-data/minimally-sufficient-pandas-a8e67f2a2428
+http://pandas.pydata.org/pandas-docs/stable/getting_started/10min.html
 https://pandas.pydata.org/pandas-docs/stable/user_guide/cookbook.html
 
-'''
+# pandas cookbook data
+https://github.com/PacktPublishing/Pandas-Cookbook/tree/master/data
+
+
+"""
+
+import numpy as np
+import pandas as pd
+from pprint import pprint
+import timeit
+import time
+
+df = pd.DataFrame({'AAA': [4,5,6,7],
+                   'BBB': [10, 20, 30, 40],
+                   'CCC': [100, 50, -30, -50]})
+
+print(df)
+
+
+
+# minimally Sufficient Pandas
+
+
+df = pd.read_csv('data/sample_data.csv', index_col=0)
+
+df['state']
+
+col_select = 'favorite food'
+df[col_select]
+df['favorite food']
+
+# loc by label
+# iloc by index
+
+
+
+# for performance gains use numpy instead of pandas
+
+college = pd.read_csv('data/college.csv')
+college.head()
+college.columns = [col.lower() for col in college.columns]
+
+
+college[college.isna().any(axis=1)]
+
+# only us isna and notna
+df.isna()
+df.notna()
+df.notna().all(axis=1)
+df.notna().all(axis=0)
+
+
+df.isna().all(axis=1)
+df.isna().any(axis=1)
+
+college['ugds'] + 100
+
+# returns new df with axis set
+college_idx = college.set_index('instnm')
+college_idx.head()
+# just select columns wanted and drop nas
+sats = college_idx[['satmtmid', 'satvrmid']].dropna()
+sats.head()
+
+
+# adding
+ugds = college_idx['ugds']
+ugds.head()
+ugds_operator = ugds + 100
+ugds_method = ugds.add(100)
+
+ugds_operator.head() == ugds_method.head()
+ugds_operator.equals(ugds_method)
+
+
+# z score
+sat_mean = sats.mean()
+sat_mean
+sat_std = sats.std()
+sat_std
+
+
+z_op = (sats - sat_mean) / sat_std
+z_met = sats.sub(sat_mean).div(sat_std)
+z_op.equals(z_met)
+
+college.columns
+
+college_race = college_idx.loc[:, 'ugds_white':'ugds_unkn']
+college_race.head()
+
+df_attempt = college_race * ugds
+df_attempt.shape
+
+ugds.head()
+df_correct = college_race.mul(ugds, axis='index').round(0)
+df_correct.head()
+
+
+"""
++ - add
+- - sub and subtract
+* - mul and multiply
+/ - div, divide and truediv
+** - pow
+// - floordiv
+% - mod
+
+> - gt
+< - lt
+>= - ge
+<= - le
+== - eq
+!= - ne
+"""
+
+
+college_idx.iloc[0, ]
+college_idx.iloc[0:1, ]
+college_idx.iloc[:1, ]
+college_idx.iloc[:1, :4]
+college_idx.loc[college_idx.index[:5], ['city', 'stabbr']]
+
+
+
+college_idx[['city', 'stabbr', 'hbcu']].head()
+college_idx.loc[:, ['city', 'stabbr', 'hbcu']].head()
+
+college.loc[:5, ['instnm', 'city', 'stabbr', 'hbcu']]
+college[['instnm', 'city', 'stabbr', 'hbcu']]
+
+
+# use built in methods
+ugds = college['ugds'].dropna()
+ugds.head()
+ugds.sum()
+sum(ugds)
+ugds.max()
+max(ugds)
+
+
+# groupby
+
+college[['stabbr', 'satmtmid', 'satvrmid', 'ugds']].head()
+college[('stabbr', 'satmtmid', 'satvrmid', 'ugds')].head() # breaks
+college[{'stabbr', 'satmtmid', 'satvrmid', 'ugds'}].head() # works
+
+# 1
+college.groupby('stabbr').agg({'satmtmid': 'max'}).head()
+
+college.groupby('stabbr').agg({'satmtmid': ['min', 'max'],
+                               'satvrmid': ['min', 'max'],
+                               'ugds': 'mean'}).head()
+
+
+
+agg_dict = {'satmtmid': ['min', 'max'],
+            'satvrmid': ['min', 'max'],
+            'ugds': 'mean'}
+
+df = college.groupby(['stabbr', 'relaffil']).agg(agg_dict)
+df.columns = ['_'.join(col).strip() for col in df.columns.values]
+df.reset_index(inplace=True)
+df.head()
+
+# left off here "The similarity between groupby, pivot_table, and crosstab"
+# https://medium.com/dunder-data/minimally-sufficient-pandas-a8e67f2a2428
+
+emp = pd.read_csv('data/employee.csv')
+emp.head()
+
+
+
+
+
+
+
+
+
+
+
+
+
